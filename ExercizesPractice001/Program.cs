6b49c2c9e585e913791380eda8af7d73bc163063
Your Name <you@example.com>
2023-02-04 16:46:39 +0300

    hw 005

--- ExercizesPractice001/Program.cs
+++ ExercizesPractice001/Program.cs
@@ -289,36 +289,64 @@
 
 
 
+// Задайте массив из 12 элементов, 
+// заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных элементов массива.
 
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
 
-
-
-
-// // Задайте массив из 12 элементов, 
-// // заполненный случайными числами из промежутка [-9, 9]. 
-// // Найдите сумму отрицательных элементов массива.
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//       System.Console.Write($"input an {i + 1} element: ");
+//       array[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
 
 // int GetSumOfNegative(int[] array)
 // {
 //     int sum = 0;
-
-//     for (int i = 0; i < array.Length; i++)
+//     for(int i = 0; i < array.Length; i++)
 //     {
 //         if(array[i] < 0)
-//         sum += array[i];
+//         {
+//             sum += array[i];
+//         }
 //     }
 //     return sum;
 // }
 
-// Console.Write("Input a length of array: ");
+// void ShowArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//         System.Console.Write(array[i] + " | ");
+//     System.Console.WriteLine();
+// }
+
+
+
+// System.Console.Write("input a length of array: ");
 // int length = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input a min possible value: ");
+// System.Console.Write("Input a min possible value: ");
 // int min = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input a max possible value: ");
+// System.Console.Write("Input a min value: ");
 // int max = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input a max value: ");
 
 // int[] newArray = CreateRandomArray(length, min, max);
 // ShowArray(newArray);
+
 
 
 
---
+++ Seminar005/Program.cs
@@ -0,0 +1,145 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+// void RandomNumbers(int [] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(99, 1000);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int size = ReadInt("Input a size of massive: ");
+// int [] numbers = new int[size];
+
+// RandomNumbers(numbers);
+// PrintArray(numbers);
+// int result = 0;
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] % 2 == 0)
+//     {
+//         result++;
+//     }
+// }
+// if (result % 10 == 1)
+// {
+//     Console.WriteLine($"In the massive even number is {result}");
+// }
+// if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
+// {
+//     Console.WriteLine($"In the massive even numbers are {result}");
+// }
+// else
+
+//     Console.WriteLine($"In the massive even numbers are {result}");
+
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+// Console.WriteLine("Enter the size of the massive:  ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Massive: ");
+// PrintArray(numbers);
+// int sum = 0;
+
+// for (int z = 0; z < numbers.Length; z+=2)
+//     sum = sum + numbers[z];
+
+//     Console.WriteLine($"In total {numbers.Length}, Summary of elements that belongs to uneven positions = {sum}");
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = new Random().Next(1,10);
+//         }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+// Console.WriteLine("Enter the size of the massive  ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// double[] numbers = new double[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Massive: ");
+// PrintArray(numbers);
+// double min = Int32.MaxValue;
+// double max = Int32.MinValue;
+
+// for (int z = 0; z < numbers.Length; z++)
+// {
+//     if (numbers[z] > max)
+//         {
+//             max = numbers[z];
+//         }
+//     if (numbers[z] < min)
+//         {
+//             min = numbers[z];
+//         }
+// }
+
+// Console.WriteLine($"In total {numbers.Length} numbers. Max value = {max}, Min value = {min}");
+// Console.WriteLine($"Difference between max and min value = {max - min}");
+
+// void FillArrayRandomNumbers(double[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+//         }
+// }
+// void PrintArray(double[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
