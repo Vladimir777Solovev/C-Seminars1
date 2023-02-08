8736835dce3c74d72e3bae7696857014ec880b24
Your Name <you@example.com>
2023-02-08 15:33:44 +0300

    hw 006

---
+++ Seminar006/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+// Console.Write("Enter the numbers through a comma: ");
+// int[] numbers = StringToNum(Console.ReadLine());
+// PrintArray(numbers);
+// int sum = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > 0)
+//     {
+//         sum++;
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine($"summary of numbers bigger then 0 = {sum}");
+
+
+// int[] StringToNum(string input)
+// {
+//     int count = 1;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ',')
+//         {
+//             count++;
+//         }
+//     }
+
+//     int[] numbers = new int [count];
+//     int index = 0;
+
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         string temp = "";
+
+//         while (input [i] != ',')
+//         {
+//         if(i != input.Length - 1)
+//         {
+//             temp += input [i].ToString();
+//             i++;
+//         }
+//         else
+//         {
+//             temp += input [i].ToString();
+//             break;
+//         }
+//         }
+//         numbers[index] = Convert.ToInt32(temp);
+//         index++;
+//     }
+//     return numbers;
+// }
+
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.Write("]");
+// }
+
+
+
+ // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+// Console.Write("Input a number of a1: ");
+// double a1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a number of b1: ");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a number of a2: ");
+// double a2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a number of b2: ");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (-a2 + a1)/(-b1 + b2);
+// double y = b2 * x + a2;
+
+// Console.WriteLine($"Two lines will cross at X: {x}, Y: {y}");
