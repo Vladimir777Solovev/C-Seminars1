2e45db33823a8a3c1e34a8b3f6a817e97e712ded
Your Name <you@example.com>
2023-02-09 15:00:24 +0300

    hw007

---
+++ Seminar007/Program.cs
@@ -0,0 +1,112 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// int rows = ReadInt("Enter a count of rows: ");
+// int columns = ReadInt("Enter a count of columns: ");
+// double[,] numbers = new double[rows, columns];
+// FillArray2D(numbers);
+// PrintArray2D(numbers);
+
+// void FillArray2D(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(-99, 99) / 10.0;
+//         }
+//     }
+// }
+
+// void PrintArray2D(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> числа с такими индексами в массиве нет
+
+// Console.Write("Enter a number of row: ");
+// int f1 = Convert.ToInt32(Console.ReadLine()) - 1;
+// Console.Write("Enter a number of column: ");
+// int f2 = Convert.ToInt32(Console.ReadLine()) - 1;
+// int n = 5; 
+// int m = 7; 
+// Random random = new Random();
+// int[,] arr = new int[n, m];
+// Console.WriteLine("Basic massive: ");
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+// {
+//     arr[i, j] = random.Next(10, 99);
+// Console.Write("{0} ", arr[i, j]);
+// }
+// Console.WriteLine();
+// }
+//     if (f1 < 0 | f1 > arr.GetLength(0) - 1 | f2 < 0 | f2 > arr.GetLength(1) - 1)
+// {
+// Console.WriteLine("This element doesn`t exist ");
+// }
+//     else
+// {
+//     Console.WriteLine("A value of elements in massive = {0}", arr[f1, f2]);
+// }
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Random random = new Random();
+// int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = random.Next(1, 10);
+//         Console.Write(arr[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine(" ");
+// Console.WriteLine(arr.GetLength(0));
+// for (int j = 0; j < arr.GetLength(1); j++)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         sum += arr[i, j];
+//     }
+//     Console.Write($"{ sum / arr.GetLength(0)} ");
+// }
